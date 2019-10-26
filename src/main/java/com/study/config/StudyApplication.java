f5fa389fb5ac33a2f79fbb48786deac4ab995b50
gunkim0318 <gunkim0318@gmail.com>
2019-10-26 12:33:58 +0900

    프로젝트 셋팅

--- src/main/java/com/study/config/StudyApplication.java
+++ src/main/java/com/study/config/StudyApplication.java
@@ -1,13 +1,11 @@
-package com.study.controller;
+package com.study.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StudyApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(StudyApplication.class, args);
     }
-
 }
--- src/test/java/com/study/config/StudyApplicationTests.java
+++ src/test/java/com/study/config/StudyApplicationTests.java
@@ -1,4 +1,4 @@
-package com.study.controller;
+package com.study.config;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
