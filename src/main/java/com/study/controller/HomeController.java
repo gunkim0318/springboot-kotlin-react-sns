3c8c1d5ab459eedb8277a57d38c400f74cd5b883
gunkim0318 <gunkim0318@gmail.com>
2019-10-26 17:29:36 +0900

    디렉토리 설정 중

--- src/main/java/com/study/controller/HomeController.java
+++ src/main/java/com/study/controller/HomeController.java
@@ -1,14 +1,12 @@
 package com.study.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value="/")
 public class HomeController {
-    @GetMapping(value="test")
-    public  String index(){
+    @GetMapping(value="/test")
+    public String index(){
         return "Hello World!";
     }
 }
