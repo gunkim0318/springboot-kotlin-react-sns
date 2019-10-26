ee3d4c2483965773e4e1c398b3ce125f839dfdc9
gunkim0318 <gunkim0318@gmail.com>
2019-10-26 16:57:17 +0900

    설정 및 컨트롤러 테스트

--- src/main/java/com/study/controller/HomeController.java
+++ src/main/java/com/study/controller/HomeController.java
@@ -1,13 +1,14 @@
 package com.study.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@RequestMapping("/")
+@RestController
+@RequestMapping(value="/")
 public class HomeController {
-    public @ResponseBody String index(){
+    @GetMapping(value="test")
+    public  String index(){
         return "Hello World!";
     }
 }
