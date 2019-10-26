8f36bcfda0b2a9fb25f6cfcc25d8c4b3cf570cca
parkge5469 <phg5469@nate.com>
2019-10-26 11:29:19 +0900

    리액트 라우트 첫번째 작업

---
+++ deploy/src/router.js
@@ -0,0 +1,13 @@
+import LoginPage from './layout/sub/login/LoginPage';
+
+
+
+const router = [
+    {
+        path: '/Login',
+        route: 'Sub',
+        component: LoginPage,
+    }
+]
+
+export default router;
--- deploy/src/router/Sub.tsx
+++ deploy/src/router/Sub.tsx
@@ -6,77 +6,49 @@ import { WithWidth } from "@material-ui/core/withWidth";
 import { makeStyles } from "@material-ui/styles";
 import * as React from "react";
 import { connect } from "react-redux";
-import { Route, RouteComponentProps, Router } from "react-router-dom";
+import { Route, RouteComponentProps, Router, Switch } from "react-router-dom";
 import { history } from "../configureStore";
 import { RootState } from "../reducers/index";
 import withRoot from "../withRoot";
 
-interface Props extends RouteComponentProps<void>, WithWidth {}
+//Route
+import router from '../router';
+import LoginPage from '../layout/sub/login/LoginPage';
+
+interface Props extends RouteComponentProps<void>, WithWidth { }
 
 function Sub(props?: Props) {
 	const classes = useStyles();
-	const [mobileOpen, setMobileOpen] = React.useState(true);
+	
+	//useState
+	const [ routerArray ] = React.useState([...router])
 
 	return (
 		<Router history={history}>
-            {/* <Route exact={true} path="/" component={HomePage} />
-			<Route exact={true} path="/home" component={HomePage} />
-			<Route exact={true} path="/todo" component={TodoPage} /> */}
+			<Switch>
+				{console.log(routerArray.length)}
+				{
+					routerArray.map((v, i) => {
+						if('Sub' === v.route) {
+							console.log('dddddd')
+							return(
+								<Route path={v.path} key={i} component={v.component} />
+							)
+						} else {
+							return []
+						}
+					})
+				}
+				<Route expact path='/' component={LoginPage} />
+				<Route path='/sub' component={LoginPage} />
+				<Route path='/sub/' component={LoginPage} />
+			</Switch>
 		</Router>
 	);
 }
 
-
-
-const drawerWidth = 240;
 const useStyles = makeStyles((theme: Theme) => ({
-	root: {
-		width: "100%",
-		height: "100%",
-		zIndex: 1,
-		overflow: "hidden",
-	},
-	appFrame: {
-		position: "relative",
-		display: "flex",
-		width: "100%",
-		height: "100%",
-	},
-	appBar: {
-		zIndex: theme.zIndex.drawer + 1,
-		position: "absolute",
-	},
-	navIconHide: {
-		[theme.breakpoints.up("md")]: {
-			display: "none",
-		},
-	},
-	drawerHeader: theme.mixins.toolbar,
-	drawerPaper: {
-		width: 250,
-		backgroundColor: theme.palette.background.default,
-		[theme.breakpoints.up("md")]: {
-			width: drawerWidth,
-			position: "relative",
-			height: "100%",
-		},
-	},
-	content: {
-		backgroundColor: theme.palette.background.default,
-		width: "100%",
-		height: "calc(100% - 56px)",
-		marginTop: 56,
-		[theme.breakpoints.up("sm")]: {
-			height: "calc(100% - 64px)",
-			marginTop: 64,
-		},
-	},
+
 }));
 
-function mapStateToProps(state: RootState) {
-	return {
-		todoList: state.todoList,
-	};
-}
-
-export default connect(mapStateToProps)(withRoot(withWidth()(Sub)));
+export default (withRoot(withWidth()(Sub)));
