6ec667df611a438f9f9aeb230fba6e4dc3e03e11
gunkim0318 <gunkim0318@gmail.com>
2019-10-27 00:19:22 +0900

    테스트 의존성 추가 및 컨트롤러 테스트 완료

--- src/main/java/com/study/config/StudyApplication.java
+++ src/main/java/com/study/config/StudyApplication.java
@@ -2,8 +2,10 @@ package com.study.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.study.controller"})
 public class StudyApplication {
     public static void main(String[] args) {
         SpringApplication.run(StudyApplication.class, args);
--- src/main/java/com/study/controller/HomeController.java
+++ src/main/java/com/study/controller/HomeController.java
@@ -1,12 +1,25 @@
 package com.study.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+@Controller
+@RequestMapping(value = "/")
 public class HomeController {
-    @GetMapping("/test")
-    public String index(){
+    private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public void index() {
+    }
+    @RequestMapping(value = "hello", method = RequestMethod.GET)
+    public @ResponseBody  String hello(){
+        logger.info("============");
+        logger.info("Hello World!");
+        logger.info("============");
         return "Hello World!";
     }
 }
