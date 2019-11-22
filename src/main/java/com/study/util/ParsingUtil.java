b1cc941047f25a07857085a92248d28955127eee
gunkim0318 <gunkim0318@gmail.com>
2019-11-22 23:58:12 +0900

    body 값이 없을 때 처리

--- src/main/java/com/study/util/ParsingUtil.java
+++ src/main/java/com/study/util/ParsingUtil.java
@@ -39,6 +39,11 @@ public class ParsingUtil {
         Map<String, Map<String, Object>> resultMap = new HashMap<String, Map<String, Object>>();
 
         resultMap.put("header", headerList);
+        
+        if(bodyList.isEmpty()){
+            bodyPut("data", "");
+        }
+
         resultMap.put("body", bodyList);
 
         return resultMap;
