48f70e52eb9809b2dfc73cdc7701891599886d61
gunkim0318 <gunkim0318@gmail.com>
2020-09-10 02:39:27 +0900

    body 스타일 마진 제거

--- web/my-app/src/client/Root.tsx
+++ web/my-app/src/client/Root.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import App from "../App";
+import { createGlobalStyle } from 'styled-components'
 
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0px;
+  }
+`
 const Root: React.FC = () => {
   return (
       <BrowserRouter>
+          <GlobalStyle/>
           <App />
       </BrowserRouter>
   );
