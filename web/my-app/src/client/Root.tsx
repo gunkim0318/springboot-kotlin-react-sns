500221ca0d137a6b2c97b53c1807453ed89cce9e
gunkim0318 <45007556+gunkim0318@users.noreply.github.com>
2020-09-10 13:15:45 +0900

    배경색 추가

--- web/my-app/src/client/Root.tsx
+++ web/my-app/src/client/Root.tsx
@@ -1,19 +1,20 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import App from "../App";
-import { createGlobalStyle } from 'styled-components'
+import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
   body {
     margin: 0px;
+    background-color: #f1f1f1;
   }
-`
+`;
 const Root: React.FC = () => {
   return (
-      <BrowserRouter>
-          <GlobalStyle/>
-          <App />
-      </BrowserRouter>
+    <BrowserRouter>
+      <GlobalStyle />
+      <App />
+    </BrowserRouter>
   );
 };
 export default Root;
