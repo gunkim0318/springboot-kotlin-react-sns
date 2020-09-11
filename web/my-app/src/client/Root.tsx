194cf78a654af2a9f47dfb7a1aea61158d1c257a
gunkim0318 <gunkim0318@gmail.com>
2020-09-12 01:23:10 +0900

    리덕스, 리덕스 사가 설정

--- web/my-app/src/client/Root.tsx
+++ web/my-app/src/client/Root.tsx
@@ -2,6 +2,11 @@ import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import App from "../App";
 import { createGlobalStyle } from "styled-components";
+import createSagaMiddleware from "redux-saga";
+import {applyMiddleware, createStore} from "redux";
+import rootReducer from "../modules";
+import {Provider} from "react-redux";
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -9,11 +14,17 @@ const GlobalStyle = createGlobalStyle`
     background-color: #f1f1f1;
   }
 `;
+
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(sagaMiddleware)));
 const Root: React.FC = () => {
   return (
     <BrowserRouter>
-      <GlobalStyle />
-      <App />
+        <Provider store={store}>
+            <GlobalStyle />
+            <App />
+        </Provider>
     </BrowserRouter>
   );
 };
