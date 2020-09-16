e2eafd0da00f9f6141cf1cab5070f10f445f04b2
gunkim0318 <gunkim0318@gmail.com>
2020-09-17 01:13:35 +0900

    글쓰기 모달로 변경

--- web/my-app/src/components/PostsInputForm.tsx
+++ web/my-app/src/components/PostsInputForm.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Paper,
   createStyles,
@@ -7,6 +7,9 @@ import {
   TextField,
   Button,
 } from "@material-ui/core";
+import Fade from "@material-ui/core/Fade";
+import Modal from "@material-ui/core/Modal";
+import Backdrop from "@material-ui/core/Backdrop";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -14,28 +17,79 @@ const useStyles = makeStyles((theme: Theme) =>
       padding: "10px",
       margin: "10px 0px",
     },
-    form: {
-      marginBottom: "10px",
+    modal: {
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    paper1: {
+      backgroundColor: theme.palette.background.paper,
+      border: "2px solid #000",
+      boxShadow: theme.shadows[5],
+      padding: theme.spacing(2, 4, 3),
+      width: theme.spacing(100),
+      margin: "10px",
+    },
+    button: {
+      marginTop: "10px",
     },
   })
 );
+
 const PostsInputForm = () => {
   const classes = useStyles();
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
   return (
     <Paper className={classes.paper}>
-      <form noValidate autoComplete="off" className={classes.form}>
-        <TextField
-          id="outlined-multiline-static"
-          label="게시물을 입력해주세요"
-          multiline
-          rows={3}
-          variant="outlined"
-          fullWidth
-        />
-      </form>
-      <Button variant="outlined" color="primary">
-        작성
-      </Button>
+      <TextField
+        id="filled-required"
+        label="게시물을 입력해주세요"
+        variant="filled"
+        fullWidth
+        onClick={handleOpen}
+        disabled
+      />
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        className={classes.modal}
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={open}>
+          <div className={classes.paper1}>
+            <TextField
+              id="outlined-multiline-static"
+              label="글 내용"
+              multiline
+              rows={4}
+              variant="outlined"
+              fullWidth
+            />
+            <Button
+              variant="contained"
+              color="primary"
+              fullWidth
+              className={classes.button}
+            >
+              등록
+            </Button>
+          </div>
+        </Fade>
+      </Modal>
     </Paper>
   );
 };
