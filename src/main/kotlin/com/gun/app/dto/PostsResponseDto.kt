9017324c4493dafb42bd4e957d67e5330aec2ec8
gunkim <gunkim0318@gmail.com>
2020-10-09 23:29:04 +0900

    좋아요도 포시하도록 수정

--- src/main/kotlin/com/gun/app/dto/PostsResponseDto.kt
+++ src/main/kotlin/com/gun/app/dto/PostsResponseDto.kt
@@ -3,6 +3,8 @@ package com.gun.app.dto
 import com.gun.app.domain.entity.Posts
 
 class PostsResponseDto(posts: Posts) {
+    val id: Long? = posts.id
     val contents: String = posts.contents
     val likes: Int = posts.likeTos.size
+    val username: String = posts.user.name
 }
--- web/my-app/src/components/Posts.tsx
+++ web/my-app/src/components/Posts.tsx
@@ -1,27 +1,56 @@
 import React from "react";
-import { Avatar, Paper, Box } from "@material-ui/core";
+import { Avatar, Paper, Box, Badge } from "@material-ui/core";
 import { createStyles, makeStyles } from "@material-ui/core";
+import ThumbUpAltIcon from "@material-ui/icons/ThumbUpAlt";
+import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
+import TextField from "@material-ui/core/TextField";
 
 const useStyles = makeStyles(() =>
   createStyles({
     root: {
-      padding: "10px",
       margin: "7px 0",
+    },
+    likes: {
+      textAlign: "center",
+      paddingTop: "20px",
+      paddingBottom: "10px",
+    },
+    downTap: {
+      background: "#f5f5f5",
+      textAlign: "center",
+      margin: "0px",
+      padding: "0px",
+      cursor: "pointer",
     },
   })
 );
-const Posts = () => {
+
+type PostsProps = {
+  contents: string;
+  likes: number;
+  username: string;
+};
+
+const Posts = ({ contents, likes, username }: PostsProps) => {
   const classes = useStyles();
   return (
     <Paper elevation={3} className={classes.root}>
-      <Box component="div" pt={1}>
+      <Box component="div" pt={2} pl={2} pr={2}>
         <div>
-          <Avatar component="span">N</Avatar>
+          <Avatar component="span">{username}</Avatar>
         </div>
         <Box component="div" m={1}>
-          김건입니다.
+          {contents}
         </Box>
+        <div className={classes.likes}>
+          <Badge color="secondary" badgeContent={likes} showZero>
+            <ThumbUpAltIcon />
+          </Badge>
+        </div>
       </Box>
+      <div className={classes.downTap}>
+        <ArrowDropDownIcon />
+      </div>
     </Paper>
   );
 };
