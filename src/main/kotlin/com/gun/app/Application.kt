d0563865b0ccfec5c575f0bf0b18694ba170a055
gunkim <gunkim0318@gmail.com>
2020-10-09 23:27:59 +0900

    테스트 데이터 입력

--- src/main/kotlin/com/gun/app/Application.kt
+++ src/main/kotlin/com/gun/app/Application.kt
@@ -1,25 +1,35 @@
 package com.gun.app
 
 import com.gun.app.domain.Role
+import com.gun.app.domain.entity.Posts
 import com.gun.app.domain.entity.User
+import com.gun.app.domain.repository.PostsRepository
 import com.gun.app.domain.repository.UserRepository
 import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+import java.util.stream.IntStream
 
 @EnableJpaAuditing
 @SpringBootApplication
 class Application{
     @Bean
-    fun runner(userRepository: UserRepository) = CommandLineRunner {
+    fun runner(userRepository: UserRepository, postsRepository: PostsRepository) = CommandLineRunner {
         val user: User = User(null,
                 "gunkim",
                 "gunkim0318@gmail.com",
                 Role.USER
                 )
         userRepository.save(user)
+
+        IntStream.rangeClosed(1, 15).forEach { i ->
+            postsRepository.save(Posts(null,
+                    "${i}번째 게시글입니다.",
+                    user
+            ))
+        }
     }
 }
 
