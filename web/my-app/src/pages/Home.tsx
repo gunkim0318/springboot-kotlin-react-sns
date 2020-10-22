f3163ababc9fbd767a80fb0da7b18156366b30e6
gunkim0318 <gunkim0318@gmail.com>
2020-10-22 23:14:57 +0900

    디자인 수정

--- web/my-app/src/pages/Home.tsx
+++ web/my-app/src/pages/Home.tsx
@@ -1,8 +1,32 @@
-import { Grid, Typography, Input, Paper } from "@material-ui/core";
+import { Grid, Typography, Input } from "@material-ui/core";
 import React from "react";
+import Timeline from '@material-ui/lab/Timeline';
+import TimelineItem from '@material-ui/lab/TimelineItem';
+import TimelineSeparator from '@material-ui/lab/TimelineSeparator';
+import TimelineConnector from '@material-ui/lab/TimelineConnector';
+import TimelineContent from '@material-ui/lab/TimelineContent';
+import TimelineDot from '@material-ui/lab/TimelineDot';
+import Paper from "@material-ui/core/Paper/Paper";
+import makeStyles from "@material-ui/core/styles/makeStyles";
+import FastfoodIcon from '@material-ui/icons/Fastfood';
+import SettingsIcon from '@material-ui/icons/Settings';
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    padding: '6px 16px',
+    width: '100%'
+  },
+  secondaryTail: {
+    backgroundColor: theme.palette.secondary.main,
+  },
+}));
 const Home: React.FC = () => {
+  const classes = useStyles();
   return (
     <>
+      <Grid container direction="row" justify="flex-end" alignItems="center">
+        <Grid style={{padding:10}}><SettingsIcon/></Grid>
+      </Grid>
       <Grid
         container
         direction="row"
@@ -10,10 +34,10 @@ const Home: React.FC = () => {
         alignItems="center"
         style={{ padding: 20 }}
       >
-        <Typography variant="h2">글을 입력하세요</Typography>
+        <Typography variant="h4">글을 입력하세요</Typography>
       </Grid>
       <Grid container direction="row" justify="center" alignItems="center">
-        <Grid xs={4}>
+        <Grid xs={5}>
           <Input
             inputProps={{ "aria-label": "description" }}
             fullWidth={true}
@@ -21,11 +45,30 @@ const Home: React.FC = () => {
           />
         </Grid>
       </Grid>
-
-      <Grid container direction="row" justify="center" alignItems="center">
-        <Paper variant="outlined" style={{ marginTop: 30 }}>
-          안녕
-        </Paper>
+      <Grid container direction="row" justify="flex-start">
+        <Grid xs={8}>
+          <Timeline align="left">
+            <TimelineItem>
+              <TimelineSeparator>
+                <TimelineDot>
+                  <FastfoodIcon />
+                </TimelineDot>
+                <TimelineConnector />
+              </TimelineSeparator>
+              <TimelineContent>
+                <Paper elevation={1} className={classes.paper} >
+                  <Typography variant="h6" component="h6">
+                    난 이걸 먹었오
+                  </Typography>
+                  <Typography>꽈배기랑 소세지빵을 먹었오</Typography>
+                </Paper>
+                <Typography variant="body2" color="textSecondary">
+                  20-10-22 9:30 am
+                </Typography>
+              </TimelineContent>
+            </TimelineItem>
+          </Timeline>
+        </Grid>
       </Grid>
     </>
   );
