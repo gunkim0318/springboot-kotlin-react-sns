dbf0bd5f96118fddb1aa955cd1eb14a88960b2c1
gunkim <gunkim0318@gmail.com>
2020-10-27 17:22:44 +0900

    내용 공백 처리

--- web/my-app/src/pages/people.tsx
+++ web/my-app/src/pages/people.tsx
@@ -2,11 +2,7 @@ import React from "react";
 import Header from "../components/Header";
 
 const people: React.FC = () => {
-  return (
-    <>
-      <Header />
-    </>
-  );
+  return <></>;
 };
 
 export default people;
