71757f1350a7e93c25430b5a77e14556b2f0c998
gunkim <gunkim0318@gmail.com>
2020-11-30 23:14:15 +0900

    프로필 이미지 사이즈 수정

--- web/my-app/src/components/organisms/Profile.tsx
+++ web/my-app/src/components/organisms/Profile.tsx
@@ -11,9 +11,9 @@ type ProfileProps = {
 const Profile = ({ data, loading, error }: ProfileProps) => {
   const { nickname, image, info, followers, following } = data;
   return (
-    <Paper style={{ padding: "40px" }} elevation={3}>
+    <Paper style={{ padding: "40px"}} elevation={3}>
       <Grid container direction="column" justify="center" alignItems="center">
-        <ProfileImage url={image} name={nickname} width={200} height={200} />
+        <ProfileImage url={image} name={nickname} width={250} height={250} />
         <Typography variant="h4">{nickname}</Typography>
         <Typography variant="body1">{info}</Typography>
         <div>
