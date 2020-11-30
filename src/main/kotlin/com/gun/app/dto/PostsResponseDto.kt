d9eba4a6c9e67811439a23ef09b94c46eb4b601e
gunkim <gunkim0318@gmail.com>
2020-11-30 23:15:19 +0900

    이미지 가져오도록 수정

--- src/main/kotlin/com/gun/app/dto/PostsResponseDto.kt
+++ src/main/kotlin/com/gun/app/dto/PostsResponseDto.kt
@@ -9,6 +9,7 @@ class PostsResponseDto(posts: Posts, user: User) {
     val contents: String = posts.contents
     val likeCnt: Int = posts.likes.size
     val isLikes: Boolean = posts.likes.contains(user)
-    val name: String = posts.user.name
+    val name: String? = posts.user.profile?.nickname
+    val image: String? = user.profile?.image
     val creDate: String = posts.createdDate.format(java.time.format.DateTimeFormatter.ofPattern("yyyy년 MM월 dd일 HH:mm:ss"))
 }
