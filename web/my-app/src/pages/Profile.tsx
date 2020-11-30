0653a2e9935ef11c219572605ee0545bf588a134
gunkim <gunkim0318@gmail.com>
2020-11-30 23:12:47 +0900

    프로필 게시글도 가져오도록 수정

--- web/my-app/src/pages/Profile.tsx
+++ web/my-app/src/pages/Profile.tsx
@@ -1,12 +1,19 @@
 import React from "react";
 import MyHeader from "../components/organisms/MyHeader";
 import { ProfileTemplate } from "../components/templates/ProfileTemplate";
+import PostsListContainer from "../containers/PostsListContainer";
+import {Grid} from "@material-ui/core";
 
 const Profile = () => {
   return (
     <>
       <MyHeader />
       <ProfileTemplate />
+      <Grid container direction="row" justify="center" alignContent="center" alignItems="center">
+          <Grid xs={5}>
+              <PostsListContainer/>
+          </Grid>
+      </Grid>
     </>
   );
 };
